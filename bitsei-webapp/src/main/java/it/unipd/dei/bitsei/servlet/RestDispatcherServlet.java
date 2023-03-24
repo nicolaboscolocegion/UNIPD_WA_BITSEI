b36e45bf33be938e2df7824a33c49b6c61961584
Farzad Shami <farzad.shami@studenti.unipd.it>
2023-03-24 14:39:00 +0100

    init(rest): Dispatches the request to the proper REST resource

---
+++ bitsei-webapp/src/main/java/it/unipd/dei/bitsei/servlet/RestDispatcherServlet.java
@@ -0,0 +1,113 @@
+package it.unipd.dei.bitsei.servlet;
+
+import it.unipd.dei.bitsei.resources.User;
+import it.unipd.dei.bitsei.resources.LogContext;
+import it.unipd.dei.bitsei.resources.Message;
+import it.unipd.dei.bitsei.rest.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Dispatches the request to the proper REST resource.
+ */
+public final class RestDispatcherServlet extends AbstractDatabaseServlet {
+
+    /**
+     * The JSON UTF-8 MIME media type
+     */
+    private static final String JSON_UTF_8_MEDIA_TYPE = "application/json; charset=utf-8";
+
+    @Override
+    protected void service(final HttpServletRequest req, final HttpServletResponse res) throws IOException {
+
+        LogContext.setIPAddress(req.getRemoteAddr());
+
+        final OutputStream out = res.getOutputStream();
+
+        try {
+
+            // if the requested resource was an User, delegate its processing and return
+            if (processUser(req, res)) {
+                return;
+            }
+
+            // if none of the above process methods succeeds, it means an unknown resource has been requested
+            LOGGER.warn("Unknown resource requested: %s.", req.getRequestURI());
+
+            final Message m = new Message("Unknown resource requested.", "E4A6",
+                    String.format("Requested resource is %s.", req.getRequestURI()));
+            res.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            res.setContentType(JSON_UTF_8_MEDIA_TYPE);
+            m.toJSON(out);
+        } catch (Throwable t) {
+            LOGGER.error("Unexpected error while processing the REST resource.", t);
+
+            final Message m = new Message("Unexpected error.", "E5A1", t.getMessage());
+            res.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            m.toJSON(out);
+        } finally {
+
+            // ensure to always flush and close the output stream
+            if (out != null) {
+                out.flush();
+                out.close();
+            }
+
+            LogContext.removeIPAddress();
+        }
+    }
+
+
+    /**
+     * Checks whether the request if for an {@link User} resource and, in case, processes it.
+     *
+     * @param req the HTTP request.
+     * @param res the HTTP response.
+     * @return {@code true} if the request was for an {@code User}; {@code false} otherwise.
+     * @throws Exception if any error occurs.
+     */
+    private boolean processUser(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+
+        final String method = req.getMethod();
+
+        String path = req.getRequestURI();
+        Message m = null;
+
+        // the requested resource was not an user
+        if (path.lastIndexOf("rest/user") <= 0) {
+            return false;
+        }
+
+        // strip everything until after the /user
+        path = path.substring(path.lastIndexOf("user") + 8);
+
+        // the request URI is: /user
+        // if method GET, list users
+        // if method POST, create user
+        if (path.length() == 0 || path.equals("/")) {
+
+            switch (method) {
+                case "GET":
+                    new ListUserRR(req, res, getConnection()).serve();
+                    break;
+                case "POST":
+//                    new CreateUserRR(req, res, getConnection()).serve();
+                    break;
+                default:
+                    LOGGER.warn("Unsupported operation for URI /user: %s.", method);
+
+                    m = new Message("Unsupported operation for URI /user.", "E4A5",
+                            String.format("Requested operation %s.", method));
+                    res.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+                    m.toJSON(res.getOutputStream());
+                    break;
+            }
+        }
+
+        return true;
+
+    }
+}
