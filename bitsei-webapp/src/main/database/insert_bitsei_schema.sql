894e25e3d5f99bc503df9b84a43b4702abb5338b
fabio_zanini <fabio.zanini@studenti.unipd.it>
2023-03-29 16:55:40 +0200

    Started writing insert file with toy data to be inserted in database

---
+++ bitsei-webapp/src/main/database/insert_bitsei_schema.sql
@@ -0,0 +1,33 @@
+-- Insert data into BankAccount table
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (1, 'HU84 4809 1474 9251 5167 0177 0342', 'Qwest Corporation', 'Hemostatic Solution', 1);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (2, 'RS22 5895 7411 5252 7229 26', null, 'topiramate', 2);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (3, 'NO73 5402 6602 729', 'Cementos Pacasmayo S.A.A.', 'SPEEDGEL RX', 3);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (4, 'LB61 9430 3ZRZ FRZC SIUA 9VSA A4IL', 'K12 Inc', 'acid reducer', 4);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (5, 'GL11 6827 6555 2299 34', '51job, Inc.', 'Thyroplex', 5);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (6, 'SM50 W266 5738 4281 RGO7 R3KC VRL', 'FuelCell Energy, Inc.', 'Hemorrhoidal', 6);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (7, 'GR98 8955 427V 64YL QUNE VQZW VZ8', 'Rice Energy Inc.', 'good sense childrens pain and fever', 7);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (8, 'PK58 ELLX SRH7 I4Q8 ICBU QY3X', 'Blackrock New York Municipal Bond Trust', 'Ben E.Keith', 8);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (9, 'SA02 95AQ HYWT 7UCA K2Y5 VCW4', 'Pieris Pharmaceuticals, Inc.', 'Mosquito', 9);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (10, 'CY63 1067 4838 NS9S GIRG D4XN 5WPL', 'Mannatech, Incorporated', 'Sinusalia', 10);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (11, 'EE82 9569 0909 4014 1244', 'Renren Inc.', 'Clopidogrel', 11);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (12, 'IT63 M020 2447 050Y GSQ1 YNM8 W3Z', 'Legg Mason Developed EX-US Diversified Core ETF', 'Maybelline Dream Pure BB Beauty Balm', 12);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (13, 'LV07 LVEK JZR9 W7TC 8RDW N', 'Bank of Commerce Holdings (CA)', 'Shopko Antibacterial Foaming Hand Sanitizer', 13);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (14, 'CZ49 2611 0825 9488 9437 9977', 'Qualstar Corporation', 'Sudan Grass', 14);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (15, 'SM27 X090 4118 107O XQPG JWYR OYF', 'Cogent Communications Holdings, Inc.', 'Propranolol Hydrochloride', 15);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (16, 'BH34 WGUG QXLR HZN0 ZLJA UG', 'Costco Wholesale Corporation', 'FML FORTE', 16);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (17, 'IT15 R487 5778 062G EWCC UMLT U9R', 'Matlin & Partners Acquisition Corporation', 'Ropinirole', 17);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (18, 'FR82 4925 2890 37UL ORS6 N1BC H61', 'Immune Design Corp.', 'Warfarin Sodium', 18);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (19, 'PS79 PJNS UOPL QV3Z W7NC WHK1 0UG9 Q', 'Peabody Energy Corporation', 'Tussin CF', 19);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (20, 'FR53 0883 8769 397G 28UJ XS77 G59', 'PowerShares LadderRite 0-5 Year Corporate Bond Portfolio', 'Vitiligo', 20);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (21, 'AL70 1915 0716 R6NF VYTO ZMX8 WTHV', 'First Trust NASDAQ Global Auto Index Fund', 'allergy relief-d', 21);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (22, 'BH56 VIXX Z0MB E5M7 EOXS RO', 'Coty Inc.', 'DIGOX', 22);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (23, 'NO56 6655 7700 422', 'Edison International', 'Promethazine Hydrochloride', 23);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (24, 'LI26 6874 242E YOX4 KOI9 7', 'Pacific Continental Corporation (Ore)', 'Sweet Potato', 24);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (25, 'VG67 BYAQ 7989 0272 0503 5433', 'Huntington Ingalls Industries, Inc.', 'Non-Drowsy Formula Wal-Tussin', 25);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (26, 'BE29 3881 3488 8359', 'EastGroup Properties, Inc.', 'Rouge Dior 740 Rosewood Serum', 26);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (27, 'LI06 7834 2YX4 WDCM L2WS R', 'Nasdaq, Inc.', 'MILK OF MAGNESIA', 27);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (28, 'MC81 5099 3340 257T CMG2 5HGL Y46', 'Nuveen California Municipal Value Fund, Inc.', 'Red Snapper', 28);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (29, 'MU94 HFEU 1991 9144 9352 6973 439S HG', 'Nuveen California Municipal Value Fund, Inc.', 'Venlafaxine', 29);
+insert into bitsei_schema.BankAccount (bankaccount_id, "IBAN", bank_name, bankaccount_friendly_name, company_id) values (30, 'KZ10 586S SR5J WKVC 2GUT', 'American Realty Investors, Inc.', 'potassium chloride', 30);
+
+-- Insert data into BankAccount table
