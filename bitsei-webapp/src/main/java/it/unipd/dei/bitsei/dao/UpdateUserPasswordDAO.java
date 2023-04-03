c2e8da4b1672592f0d4cfc6bf87b4ba0b3c7a22e
Farzad Shami <farzad.shami@studenti.unipd.it>
2023-04-03 08:21:18 +0200

    fix(reset-password): remove all tokens related to user after modifying password

--- bitsei-webapp/src/main/java/it/unipd/dei/bitsei/dao/UpdateUserPasswordDAO.java
+++ bitsei-webapp/src/main/java/it/unipd/dei/bitsei/dao/UpdateUserPasswordDAO.java
@@ -1,12 +1,10 @@
 package it.unipd.dei.bitsei.dao;
 
 import it.unipd.dei.bitsei.resources.ChangePassword;
-import it.unipd.dei.bitsei.resources.PasswordRest;
-import it.unipd.dei.bitsei.resources.User;
+
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UpdateUserPasswordDAO extends AbstractDAO<Boolean> {
@@ -14,7 +12,8 @@ public class UpdateUserPasswordDAO extends AbstractDAO<Boolean> {
     /**
      * The SQL statement to be executed
      */
-    private static final String STATEMENT = "UPDATE bitsei_schema.\"Owner\" SET password = ? WHERE owner_id = (Select owner_id from bitsei_schema.\"Password_Reset_Token\" WHERE token = ? and token_expiry > now())";
+    private static final String UPDATE_STATEMENT = "UPDATE bitsei_schema.\"Owner\" SET password = ? WHERE owner_id = (Select owner_id from bitsei_schema.\"Password_Reset_Token\" WHERE token = ? and token_expiry > now())";
+    private static final String DELETE_STATEMENT = "DELETE FROM bitsei_schema.\"Password_Reset_Token\" WHERE owner_id IN (SELECT owner_id FROM bitsei_schema.\"Password_Reset_Token\" WHERE token = ?)";
 
     private final String token;
     private final String password;
@@ -36,22 +35,38 @@ public class UpdateUserPasswordDAO extends AbstractDAO<Boolean> {
 
     @Override
     protected final void doAccess() throws SQLException {
-
         PreparedStatement pstmt = null;
         int rs = 0;
 
         // the result of the search
         boolean is_done = false;
 
+        try {
 
-        pstmt = con.prepareStatement(STATEMENT);
-        pstmt.setString(1, password);
-        pstmt.setString(2, token);
+            con.setAutoCommit(false);
+            pstmt = con.prepareStatement(UPDATE_STATEMENT);
+            pstmt.setString(1, password);
+            pstmt.setString(2, token);
 
-        rs = pstmt.executeUpdate();
-        if (rs != 0) {
-            is_done = true;
-            // Todo: delete token from table
+            rs = pstmt.executeUpdate();
+            if (rs != 0) {
+                is_done = true;
+                pstmt = con.prepareStatement(DELETE_STATEMENT);
+                pstmt.setString(1, token);
+
+                pstmt.executeUpdate();
+            }
+            con.commit();
+        } catch (SQLException e) {
+            try {
+                con.rollback();
+            } catch (SQLException e1) {
+                throw e1;
+            }
+        } finally {
+            if (pstmt != null) {
+                pstmt.close();
+            }
         }
 
         LOGGER.info("Password Rest successfully fetched.");
