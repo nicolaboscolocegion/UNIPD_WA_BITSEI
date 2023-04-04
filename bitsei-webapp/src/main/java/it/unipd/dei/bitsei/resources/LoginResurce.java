dc9e1d02687a9930eefbe9c53982867287946bd2
Nicola Boscolo <nicola.boscolocegion.1@studenti.unipd.it>
2023-04-04 13:04:27 +0200

    created LoginResurce with email and password

---
+++ bitsei-webapp/src/main/java/it/unipd/dei/bitsei/resources/LoginResurce.java
@@ -0,0 +1,68 @@
+package it.unipd.dei.bitsei.resources;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+
+
+/**
+ * login resurce for authenticate the user
+ */
+public class LoginResurce extends AbstractResource {
+
+    private String email;
+    private String password;
+    /**
+     * 
+     * @param email email of the user
+     * @param password password of the user
+     */
+    public LoginResurce(String email, String password){
+        this.email = email;
+        this.password = password;
+    }
+
+    /**
+     * create the resurce from a Json
+     * @param in input stream with json file
+     */
+    public static LoginResurce fromJSON(final InputStream in){
+        String jemail=null, jpassword=null;
+
+        try{
+            final JsonParser jp = JSON_FACTORY.createParser(in);
+            
+            while (jp.nextToken() != JsonToken.END_OBJECT) {
+
+                if (jp.getCurrentToken() == JsonToken.FIELD_NAME) {
+                    switch (jp.getCurrentName()) {
+                        case "email":
+                            jp.nextToken();
+                            jemail = jp.getText();
+                            break;
+                        case "password":
+                            jp.nextToken();
+                            jpassword = jp.getText();
+                            break;
+                    }
+                }
+            }
+
+        }catch(Exception e){
+
+        }
+
+        
+
+        return new LoginResurce(jemail, jpassword);
+    }
+
+    @Override
+    protected void writeJSON(OutputStream out) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("");
+    }
+    
+}
