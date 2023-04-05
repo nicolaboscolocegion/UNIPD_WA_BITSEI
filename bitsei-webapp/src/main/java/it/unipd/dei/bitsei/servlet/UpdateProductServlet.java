7a9d37eee772a785f0ad45e5d39e06e3468f8704
fabio_zanini <fabio.zanini@studenti.unipd.it>
2023-04-05 15:57:14 +0200

    Fixed documentation in CreateProductServlet.java and added UpdateProductDAAO.java and UpdateProductServlet.java which implement the updating for the entity Product

--- bitsei-webapp/src/main/java/it/unipd/dei/bitsei/servlet/UpdateProductServlet.java
+++ bitsei-webapp/src/main/java/it/unipd/dei/bitsei/servlet/UpdateProductServlet.java
@@ -1,6 +1,7 @@
 package it.unipd.dei.bitsei.servlet;
 
-import it.unipd.dei.bitsei.dao.CreateProductDAO;
+
+import it.unipd.dei.bitsei.dao.UpdateProductDAO;
 import it.unipd.dei.bitsei.resources.LogContext;
 import it.unipd.dei.bitsei.resources.Message;
 import it.unipd.dei.bitsei.resources.Product;
@@ -15,15 +16,16 @@ import java.sql.SQLException;
 import static it.unipd.dei.bitsei.utils.RegexValidationClass.fieldRegexValidation;
 
 /**
- * Creates a new product into the database.
+ * Updates a product into the database.
  *
  * @author Fabio Zanini (fabio.zanini@studenti.unipd.it)
  * @version 1.00
  * @since 1.00
  */
-public final class CreateProductServlet extends AbstractDatabaseServlet {
+public final class UpdateProductServlet extends AbstractDatabaseServlet {
+
     /**
-     * Creates a new product into the database.
+     * Updates the product into the database.
      *
      * @param req the HTTP request from the client.
      * @param res the HTTP response from the server.
@@ -31,7 +33,7 @@ public final class CreateProductServlet extends AbstractDatabaseServlet {
      * @throws ServletException if any error occurs while executing the servlet.
      * @throws IOException if any error occurs in the client/server communication.
      */
-    public void doPost(HttpServletRequest req, HttpServletResponse res)
+    public void doPut(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
 
         LogContext.setIPAddress(req.getRemoteAddr());
@@ -40,7 +42,8 @@ public final class CreateProductServlet extends AbstractDatabaseServlet {
         Product p = null;
         Message m = null;
 
-        int company_id = 1;
+        int product_id = -1;
+        int company_id = -1;
         String title = null;
         int default_price = 1;
         String logo = null;
@@ -50,51 +53,63 @@ public final class CreateProductServlet extends AbstractDatabaseServlet {
         try {
 
             // retrieves the request parameters
-            company_id = Integer.valueOf(req.getParameter("company_id"));
+            String company_id_raw = req.getParameter("company_id");
+            try {
+                company_id = Integer.valueOf(company_id_raw);
+            }
+            catch (NumberFormatException ex) {
+                LOGGER.info("No company id provided for %s, will be set to null.", title);
+            }
+            //company_id = Integer.valueOf(req.getParameter("company_id"));
             title = req.getParameter("title");
             default_price = Integer.valueOf(req.getParameter("default_price"));
             logo = req.getParameter("logo");
             measurement_unit = req.getParameter("measurement_unit");
             description = req.getParameter("description");
 
+            String product_id_raw = req.getParameter("product_id");
+            try {
+                product_id = Integer.valueOf(product_id_raw);
+            }
+            catch (NumberFormatException ex) {
+                LOGGER.error("Error while passing product %s", title);
+            }
+
             LOGGER.info("DATA: compID: " + company_id + " title: " + title + " defPrice: " + default_price + " logo: " + logo + " measurUnit: " + measurement_unit + " descr: " + description);
-
-
 
             fieldRegexValidation("[^\\s]+(\\.(?i)(jpg|jpeg|png|gif|bmp))$", logo, "LOGO");
 
+            // creates a new foo customer
+            p = new Product(product_id, company_id, title, default_price, logo, measurement_unit, description);
 
-            // creates a new foo product
-            p = new Product(company_id, title, default_price, logo, measurement_unit,  description);
+            // creates a new object for accessing the database and stores the customer
+            new UpdateProductDAO(getConnection(), p).access();
 
-            // creates a new object for accessing the database and stores the product
-            new CreateProductDAO(getConnection(), p).access();
+            m = new Message(String.format("Product %s successfully updated.", title));
 
-            m = new Message(String.format("Product %s successfully created.", title));
-
-            LOGGER.info("Product %s successfully created in the database.", title);
+            LOGGER.info("Product %s successfully updated.", title);
 
         } catch (NumberFormatException ex) {
             m = new Message(
-                    "Cannot create the product. Invalid input parameters: company_id and default_price must be integers, and title, logo, measurement_unit and description must be string.",
+                    "Cannot update the product. Invalid input parameters: company_id and default_price must be integers, and title, logo, measurement_unit and description must be string.",
                     "E100", ex.getMessage());
 
             LOGGER.error(
-                    "Cannot create the product. Invalid input parameters: company_id and default_price must be integers, and title, logo, measurement_unit and description must be string.",
+                    "Cannot update the product. Invalid input parameters: company_id and default_price must be integers, and title, logo, measurement_unit and description must be string.",
                     ex);
         } catch (SQLException ex) {
             if ("23505".equals(ex.getSQLState())) {
-                m = new Message(String.format("Cannot create the product: product %s already exists.", title), "E300",
+                m = new Message(String.format("Cannot update the product: product %s already exists.", title), "E300",
                         ex.getMessage());
 
                 LOGGER.error(
-                        new StringFormattedMessage("Cannot create the product: product %s already exists.", title),
+                        new StringFormattedMessage("Cannot update the product: product %s already exists.", title),
                         ex);
             } else {
-                m = new Message("Cannot create the product: unexpected error while accessing the database.", "E200",
+                m = new Message("Cannot update the product: unexpected error while accessing the database.", "E200",
                         ex.getMessage());
 
-                LOGGER.error("Cannot create the product: unexpected error while accessing the database.", ex);
+                LOGGER.error("Cannot update the product: unexpected error while accessing the database.", ex);
             }
         }  catch (IllegalArgumentException ex) {
             m = new Message(
@@ -105,8 +120,12 @@ public final class CreateProductServlet extends AbstractDatabaseServlet {
                     "Invalid input parameters. " + ex.getMessage(), ex);
         }
 
+
         LogContext.removeIPAddress();
         LogContext.removeAction();
         LogContext.removeResource();
+
+
     }
+
 }
