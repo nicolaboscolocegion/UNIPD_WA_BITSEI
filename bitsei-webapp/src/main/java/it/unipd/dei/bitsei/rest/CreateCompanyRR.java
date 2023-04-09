f0a4c891cd32ecb6a3f2c77168ad11408912f2b7
Farzad Shami <farzad.shami@studenti.unipd.it>
2023-04-09 16:04:07 +0200

    feat(company): fix the logo retrieving and creating

--- bitsei-webapp/src/main/java/it/unipd/dei/bitsei/rest/CreateCompanyRR.java
+++ bitsei-webapp/src/main/java/it/unipd/dei/bitsei/rest/CreateCompanyRR.java
@@ -21,6 +21,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
 
 import java.io.IOException;
 import java.sql.Connection;
