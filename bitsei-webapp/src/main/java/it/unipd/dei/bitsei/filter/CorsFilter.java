c9a3e88e6becfd50a350b8eff5313f4d76e0275b
Farzad Shami <farzad.shami@studenti.unipd.it>
2023-05-13 16:58:28 +0200

    fix(CORS): Add PUT as Method

--- bitsei-webapp/src/main/java/it/unipd/dei/bitsei/filter/CorsFilter.java
+++ bitsei-webapp/src/main/java/it/unipd/dei/bitsei/filter/CorsFilter.java
@@ -71,7 +71,7 @@ public class CorsFilter implements Filter {
         HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
 
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Origin, Authorization, X-Requested-With, Content-Type, Accept, Key");
