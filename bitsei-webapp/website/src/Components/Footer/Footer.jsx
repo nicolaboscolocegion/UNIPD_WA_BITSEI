a1809ed8fc035c5c03bf5a9ba39a639c00523b34
Mirco Cazzaro <mirco.cazzaro@studenti.unipd.it>
2023-05-19 09:32:01 +0200

    added not synced files

---
+++ bitsei-webapp/website/src/Components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className="py-4 bg-light mt-auto">
+            <div className="container-fluid px-4">
+                <div className="d-flex align-items-center justify-content-between small">
+                    <div className="text-muted">Copyright © BITSEI 2023</div>
+                    <div>
+                        <a href="#">Privacy Policy</a>·
+                        <a href="#">Terms &amp; Conditions</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ bitsei-webapp/website/src/Components/Image/Image.jsx
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from "react";
+import gate from "../../gate";
+import {Buffer} from 'buffer';
+import logo from "./bitseiLogo";
+
+function Image({id}) {
+    const [image, setImage] = useState("");
+
+    useEffect(() => {
+        gate
+            .getCompanyImage(id)
+            .then((response) => {
+                // check bytes length to see if the image is empty
+                if (response.data.byteLength < 500) {
+                    setImage(logo());
+                    return;
+                }
+                setImage(Buffer.from(response.data).toString('base64'));
+            })
+            .catch(() => {
+                setImage(logo());
+            });
+    }, [id]);
+
+    return (
+        <img width="70" className="d-inline-block align-text-top"
+            src={`data:image/png;base64, ${image}`}
+            alt={"company_logo"}
+        />
+
+    );
+}
+
+export default Image;
