1ae417162e916004d9a1f340f09c88f715999af5
Farzad Shami <farzad.shami@studenti.unipd.it>
2023-05-30 15:03:31 +0200

    fix(BankAccount): Re-factor the component

--- bitsei-webapp/website/src/Components/DeleteConfirm/DeleteConfirm.jsx
+++ bitsei-webapp/website/src/Components/DeleteConfirm/DeleteConfirm.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-import {Table, Modal, Button } from "react-bootstrap";
+import {Table, Modal, Button} from "react-bootstrap";
 
 
-function DeleteConfirm({show, handleClose,handleSumbit, heading, body, item_id}) {
+function DeleteConfirm({show, handleClose, handleSubmit, heading, body, item_id}) {
     return (
         <Modal show={show} onHide={handleClose}>
-                                        <Modal.Header closeButton>
-                                        <Modal.Title>{heading}</Modal.Title>
-                                        </Modal.Header>
-                                        <Modal.Body>{body}</Modal.Body>
-                                        <Modal.Footer>
-                                        <Button variant="secondary" onClick={handleClose}>
-                                            Close
-                                        </Button>
-                                        <Button variant="danger" onClick={() => handleSumbit(item_id)}>
-                                            Delete
-                                        </Button>
-                                        </Modal.Footer>
-                                    </Modal>
+            <Modal.Header closeButton>
+                <Modal.Title>{heading}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>{body}</Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="danger" onClick={() => handleSubmit(item_id)}>
+                    Delete
+                </Button>
+            </Modal.Footer>
+        </Modal>
     );
 }
 
