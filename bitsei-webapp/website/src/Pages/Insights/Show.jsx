c7760f5ab2280e69b8b7983af458b4b395e55a93
christianmarch <christian.marchiori@studenti.unipd.it>
2023-05-30 23:32:49 +0200

    Updated insight section

--- bitsei-webapp/website/src/Pages/Insights/Show.jsx
+++ bitsei-webapp/website/src/Pages/Insights/Show.jsx
@@ -7,6 +7,7 @@ import SidebarFilter from "../../Components/SidebarFilters/SidebarFilter";
 import Button from 'react-bootstrap/Button';
 import Chart from 'chart.js/auto'
 import Nav from 'react-bootstrap/Nav';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 // TODO: Refactor the logic section of code
 // TODO: Use functions to generate the chart based on that
@@ -19,6 +20,13 @@ function ShowChart() {
     const [chartType, setChartType] = useState(1)
     const [chartPeriod, setChartPeriod] = useState(1)
     const [show, setShow] = useState(false);
+    const [showTable, setShowTable] = useState(false);
+    var count = 0;
+    const mapPeriods = {
+        1: "Months",
+        2: "Years",
+        3: "Days",
+    };
 
     useEffect(() => {
         //Process request (the payload is dataToSend) to get data for drawing chart
@@ -36,7 +44,6 @@ function ShowChart() {
     useEffect(() => {
         const ctx = document.getElementById('myChart');
         if (chart.type === 1) {
-            console.log("TYPE 1");
             const myChart = new Chart(ctx, {
                 type: 'bar',
                 data: {
@@ -55,12 +62,10 @@ function ShowChart() {
                     }
                 }
             });
-
             return () => {
                 myChart.destroy();
             };
         } else if (chart.type === 2) {
-            console.log("TYPE 2");
             const myChart = new Chart(ctx, {
                 type: 'line',
                 data: {
@@ -80,17 +85,91 @@ function ShowChart() {
                     }
                 }
             });
-
+            return () => {
+                myChart.destroy();
+            };
+        } else if (chart.type === 3) {
+            const myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: chart.labels,
+                    datasets: [{
+                        label: 'Average discount',
+                        data: chart.type,
+                        borderWidth: 1,
+                        borderColor: 'rgb(75, 192, 192)'
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+            return () => {
+                myChart.destroy();
+            };
+        } else if (chart.type === 4) {
+            console.log("Type: "+chart.type+" | Labels: "+chart.labels+" | Data: "+chart.data);
+            const myChart = new Chart(ctx, {
+                type: 'doughnut',
+                data: {
+                    labels: chart.labels,
+                    datasets: [{
+                        label: '# of Invoices',
+                        data: chart.data,
+                        borderWidth: 2,
+                        hoverOffset: 4
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+            return () => {
+                myChart.destroy();
+            };
+        } else if (chart.type === 5) {
+            console.log("Type: "+chart.type+" | Labels: "+chart.labels+" | Data: "+chart.data);
+            const myChart = new Chart(ctx, {
+                type: 'doughnut',
+                data: {
+                    labels: chart.labels,
+                    datasets: [{
+                        label: 'Total profit',
+                        data: chart.data,
+                        borderWidth: 2,
+                        hoverOffset: 4
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
             return () => {
                 myChart.destroy();
             };
         }
-
     }, [chart]);
 
     useEffect(() => {
+        count = 0;
+        setShowTable(true);
+    },[chart]);
+
+    useEffect(() => {
         setFilters();
-    }, [chartType]);
+    }, [chartType, chartPeriod]);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
@@ -102,6 +181,16 @@ function ShowChart() {
 
     const handleTabSelect = (selectedTab) => {
         setChartType(parseInt(selectedTab));
+    }
+    
+    const handlePeriodSelect = (selectedItem) => {
+        setChartPeriod(parseInt(selectedItem));
+        console.log(parseInt(selectedItem))
+    }
+
+    function buttonTitle(){
+        console.log(mapPeriods[chart.period]);
+        return mapPeriods.hasOwnProperty(chart.period) ? (mapPeriods[chart.period]) : (console.log("Error"));
     }
 
     //Request data (stored in dataToSend)
@@ -184,30 +273,20 @@ function ShowChart() {
                                                    filterByTotal={filterByTotal} filterByDiscount={filterByDiscount}
                                                    filterByPfr={filterByPfr} filterByInvoiceDate={filterByInvoiceDate}
                                                    filterByWarningDate={filterByWarningDate} setFilters={setFilters}/>
-
-                                    <Button variant="outline-primary" onClick={handleShow}>
-                                        Manage filters
-                                    </Button>
+                                    <div className="d-flex justify-content-between mt-3 mx-5">
+                                        <Button variant="outline-primary">
+                                            Download
+                                        </Button>
+                                        <Button variant="outline-primary">
+                                            Export
+                                        </Button>
+                                        <Button variant="outline-primary" onClick={handleShow}>
+                                            Manage filters
+                                        </Button>
+                                    </div>
                                 </section>
                                 <div className="card-body">
-                                    <div className="dropdown float-left">
-
-                                        <Button
-                                            onClick={() => {
-                                                console.log("filterByTotal: " + filterByTotal.isEnabled + ", " + filterByTotal.fromValue + ", " + filterByTotal.toValue +
-                                                    "\nfilterByDiscount: " + filterByDiscount.isEnabled + ", " + filterByDiscount.fromValue + ", " + filterByDiscount.toValue +
-                                                    "\nfilterByInvoiceDate: " + filterByInvoiceDate.isEnabled + ", " + filterByInvoiceDate.fromValue + ", " + filterByInvoiceDate.toValue +
-                                                    "\nfilterByWarningDate: " + filterByWarningDate.isEnabled + ", " + filterByWarningDate.fromValue + ", " + filterByWarningDate.toValue +
-                                                    "\nfilterByDiscount: " + filterByPfr.isEnabled + ", " + filterByPfr.fromValue + ", " + filterByPfr.toValue +
-                                                    "\nType: " + chartType + " Period: " + chartPeriod);
-                                            }}
-                                        >
-                                            Console Log filters
-                                        </Button>
-
-                                    </div>
                                     <div className="container-fluid">
-
                                         <Nav fill variant="tabs" activeKey={chartType} defaultActiveKey="1"
                                              onSelect={handleTabSelect}>
                                             <Nav.Item>
@@ -216,18 +295,80 @@ function ShowChart() {
                                             <Nav.Item>
                                                 <Nav.Link eventKey="2">Total by Period</Nav.Link>
                                             </Nav.Item>
+                                            <Nav.Item>
+                                                <Nav.Link eventKey="3">Discount by Period</Nav.Link>
+                                            </Nav.Item>
+                                            <Nav.Item>
+                                                <Nav.Link eventKey="4">Invoices by Customer</Nav.Link>
+                                            </Nav.Item>
+                                            <Nav.Item>
+                                                <Nav.Link eventKey="5">Total by Customer</Nav.Link>
+                                            </Nav.Item>
                                         </Nav>
 
-                                        <div className="chart-container">
-                                            <canvas id="myChart"/>
+                                        <div className="chart-container w-75 h-75 mx-auto">
+                                            <canvas id="myChart" />
                                         </div>
 
-                                        <div className="container">
+                                        <div className="dropdown mb-3">
+                                            <div className="row d-flex justify-content-end">
+                                                <div className="col col-auto d-flex align-items-center">
+                                                    Group by: 
+                                                </div>
+                                                <div className="col col-auto">
+                                                    <Dropdown onSelect={handlePeriodSelect}>
+                                                        {mapPeriods.hasOwnProperty(chart.period) ? (
+                                                            <Dropdown.Toggle variant="outline-primary" id="dropdown-basic">
+                                                                {mapPeriods[chart.period]}
+                                                            </Dropdown.Toggle>
+                                                        ) : (
+                                                            <Dropdown.Toggle variant="outline-primary" id="dropdown-basic">
+                                                                Error
+                                                            </Dropdown.Toggle>
+                                                        )}
+                                                        <Dropdown.Menu>
+                                                            <Dropdown.Item eventKey="1">Months</Dropdown.Item>
+                                                            <Dropdown.Item eventKey="2">Years</Dropdown.Item>
+                                                            <Dropdown.Item eventKey="3">Days</Dropdown.Item>
+                                                        </Dropdown.Menu>
+                                                    </Dropdown>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        {/*<div className="container">
                                             <span className="text-center">Type: {chart.type}     </span>
                                             <span className="text-center">Period: {chart.period}     </span>
                                             <span className="text-center">Labels: {chart.labels}      </span>
                                             <span className="text-center">Data: {chart.data}     </span>
+                                        </div>*/}
+                                        <h3 className="text-center mb-3">Table view</h3>
+                                        {showTable && (
+                                        <div className="table-responsive d-flex justify-content-center">
+                                            <table className="table table-hover table-bordered table-sm w-50">
+                                                <thead className="indigo lighten-5">
+                                                <tr>
+                                                    <th className="text-center">Labels</th>
+                                                    <th className="text-center">Data</th>
+                                                </tr>
+                                                </thead>
+                                                <tbody>
+                                                {
+                                                    
+                                                    (chart.labels)?.map((item) => {
+                                                        return (
+                                                            <tr>
+                                                                <td className="text-center">{item}</td>
+                                                                <td className="text-center">{chart.data[count]}</td>
+                                                            </tr>
+                                                        )
+                                                        count++;
+                                                    })
+                                                }
+                                                </tbody>
+                                            </table>
                                         </div>
+                                        )}
 
                                     </div>
                                 </div>
