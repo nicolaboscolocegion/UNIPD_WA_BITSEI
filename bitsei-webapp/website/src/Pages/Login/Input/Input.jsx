50633df6986b8104a39c987154a031e434c05618
Farzad Shami <farzad.shami@studenti.unipd.it>
2023-06-03 09:32:55 +0200

    fix(Login): add validators

--- bitsei-webapp/website/src/Pages/Login/Input/Input.jsx
+++ bitsei-webapp/website/src/Pages/Login/Input/Input.jsx
@@ -1,18 +1,33 @@
 import React from "react";
 import "../../../styles.css";
+import {errorMessages} from "../../../helpers/errorMessages";
 
-function Input({ name, register, type, error }) {
-  return (
-      <div className="form-floating mb-3">
-          <input
-              className="form-control"
-              type={type}
-              name={name}
-              {...register}
-          />
-          <label >{name}</label>
-      </div>
-  );
+function Input({name, register, type, errors}) {
+    console.log(type)
+    return (
+        <div className="form-floating mb-3">
+            <input
+                className="form-control"
+                type={type}
+                name={name}
+                {...register}
+            />
+            <label>{name}</label>
+            {
+                errors[type]
+                &&
+                <span style={{
+                    display: 'inline-block',
+                    padding: "5px 10px",
+                    backgroundColor: "#ffcccc",
+                    color: "#ff0000",
+                    borderRadius: "3px"
+                }}>
+                    {errors[type].message || errorMessages[errors[type].type] || "Not valid"}
+                </span>
+            }
+        </div>
+    );
 }
 
 export default Input;
