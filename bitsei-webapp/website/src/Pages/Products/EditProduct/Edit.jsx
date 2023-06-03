05909ab24282598af3b007499a3e093484e369e7
fabio_zanini <fabio.zanini@studenti.unipd.it>
2023-06-03 12:44:21 +0200

    Working on fixing product edit page

--- bitsei-webapp/website/src/Pages/Products/EditProduct/Edit.jsx
+++ bitsei-webapp/website/src/Pages/Products/EditProduct/Edit.jsx
@@ -15,9 +15,7 @@ function EditProduct() {
         gate
             .getProduct(product_id,company_id)
             .then(response => {
-                console.log(response.data.product);
                 const product = response.data.product;
-                console.log(product)
                 reset({
                     title: product.title,
                     default_price: product.default_price,
@@ -30,7 +28,7 @@ function EditProduct() {
             }
 
         );
-    }, [product_id, company_id]);
+    }, [product_id, company_id, reset]);
 
 
 
@@ -41,11 +39,11 @@ function EditProduct() {
 
         const formData = new FormData();
         formData.append("title", data.title);
-        formData.append("default_price", data.default_price);
+        formData.append("default_price", parseInt(data.default_price));
         formData.append("measurement_unit", data.measurement_unit);
         formData.append("description", data.description);
 
-        console.log(data)
+        console.log(formData)
 
         gate
             .editProduct({product: {companyID: parseInt(company_id), ...data}}, product_id, company_id)
@@ -61,7 +59,7 @@ function EditProduct() {
 
     const fields = [
         [{name: "title", type: "string"}, {name: "default_price", type: "int"}],
-        [{name: "measurement_unit", type: "string"}, {name: "description", type: "string"}]
+        [{name: "measurement_unit", type: "string"}, {name: "description", type: "string"}],
     ]
 
     return (
@@ -70,3 +68,30 @@ function EditProduct() {
 }
 
 export default EditProduct;
+
+    /*const submitHandler = (data, e) => {
+            e.preventDefault();
+            setPending(true);
+            gate
+                .editProduct({product: {companyID: parseInt(company_id), ...data}}, product_id, company_id)
+                .then((response) => {
+                    setPending(false)
+                })
+                .catch((error) => {
+                    toast.error("Something went wrong.");
+                })
+        };
+
+
+        const fields = [
+            [{name: "title", type: "string"}, {name: "default_price", type: "int"}],
+            [{name: "measurement_unit", type: "string"}, {name: "description", type: "string"}],
+        ]
+
+        return (
+            <Form title={"Product"} onSubmit={handleSubmit(submitHandler)} fields={fields} register={register}
+                  pending={pending}/>
+        )
+    }
+
+export default EditProduct;*/
