bc6713f136ff3d9f9996c1b100ff07f03d8a9617
Nicola Boscolo <nicola.boscolocegion.1@studenti.unipd.it>
2023-06-04 16:00:19 +0200

    feat: better CSS

--- bitsei-webapp/website/src/Pages/Company/List/List.jsx
+++ bitsei-webapp/website/src/Pages/Company/List/List.jsx
@@ -19,9 +19,6 @@ function List({getLists, setActiveCompanyId}) {
     return companies.pending ? ("Loading") : (
         <div className="container-fluid px-4">
             <h1 className="mt-4">Dashboard</h1>
-            <ol className="breadcrumb mb-4">
-                <li className="breadcrumb-item active">Dashboard</li>
-            </ol>
 
             <div className="card mb-4">
                 <div className="card-header">
