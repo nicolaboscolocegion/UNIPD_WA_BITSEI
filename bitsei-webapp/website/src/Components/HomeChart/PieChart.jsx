1e6258ba3ba6a7a449c8d789581619a1466be06d
christianmarch <christian.marchiori@studenti.unipd.it>
2023-06-04 16:23:14 +0200

    Minor fixes

--- bitsei-webapp/website/src/Components/HomeChart/PieChart.jsx
+++ bitsei-webapp/website/src/Components/HomeChart/PieChart.jsx
@@ -4,7 +4,7 @@ import { Pie } from "react-chartjs-2";
 
 function PieChart({ chartData, textData }) {
     return (
-        <div className="chart-container">
+        <div className="chart-container mt-1">
             <h3 style={{ textAlign: "center" }}>{textData}</h3>
             <Pie data={chartData} />
         </div>
