19675c30bb6789538efa0fbc44566a0622e94442
Andrea <andrea@localhost.it>
2023-06-05 11:48:39 +0200

    fix(form): remove extra padding

--- bitsei-webapp/website/src/Components/Form/Form.jsx
+++ bitsei-webapp/website/src/Components/Form/Form.jsx
@@ -7,7 +7,7 @@ import {errorMessages} from "../../helpers/errorMessages";
 
 function Form({title, onSubmit, fields, register, errors = {}, pending = false, children}) {
     return (
-        <div className="container-fluid px-1 py-5 mx-auto">
+        <div className="container-fluid py-5 mx-auto">
             <div className="row d-flex justify-content-center">
                 <div className="col-xl-7 col-lg-8 col-md-9 col-11 text-center">
                     <h2>{title}</h2>
